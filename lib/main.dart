c3a7f54aea5050bd4649891fa3178f45498b905e
universal developer <69350489+universal-developer@users.noreply.github.com>
2022-07-11 22:06:53 +0300

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_nike_storev2_app/colors.dart';
 import 'package:flutter_nike_storev2_app/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -9,8 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const HomeScreen(),
-    );
+        theme: ThemeData(
+          useMaterial3: true,
+          scaffoldBackgroundColor: backgroundColor,
+        ),
+        home: const HomeScreen());
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_nike_storev2_app/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,8 +13,33 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: const Icon(
+                CupertinoIcons.text_alignleft,
+              ),
+              onPressed: () {},
+              color: blackColor,
+              splashRadius: 20,
+            ),
+            const Text('NIKE')
+          ],
+        ),
+      ),
       body: Container(
-        child: const Text('hello world'),
+        child: const Center(
+          child: Text(
+            '👋🏻',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+            ),
+          ),
+        ),
       ),
     );
   }
